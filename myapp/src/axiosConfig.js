d0ea9816f68a85329aa765cb10efe49bc5ee4832
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-05-04 12:28:51 +0530

    Added deployed url of backend

--- myapp/src/axiosConfig.js
+++ myapp/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://awdiz-10-backend.onrender.com/api/v1",
   withCredentials: true,
 });
 
